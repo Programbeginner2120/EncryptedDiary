40e18795dd79484721b0a4b212c073ef3ec1e9f1
Matthew Riley Killeen <Matthewkilleen99@gmail.com>
2021-12-14 18:51:13 -0500

    made some comments

--- app/src/main/java/EncryptedDiary/app/DiaryCipher.java
+++ app/src/main/java/EncryptedDiary/app/DiaryCipher.java
@@ -11,19 +11,22 @@ import java.security.NoSuchAlgorithmException;
 
 public class DiaryCipher {
 
-    private final String UNICODE_FORMAT = "UTF-8";
+    private final String UNICODE_FORMAT = "UTF-8";  // specifying unicode
 
     private SecretKey myKey;
 
 
+    // Basic constructor
     public DiaryCipher(){
         SecretKey myKey = this.generateSecretKey("AES");
     }
 
+    // get private instance variable myKey
     public SecretKey getKey() {
         return myKey;
     }
 
+    // Generating secret key as specified by string encryptionType
     private SecretKey generateSecretKey(String encryptionType) {
         try {
             KeyGenerator keyGen = KeyGenerator.getInstance(encryptionType);
@@ -35,6 +38,7 @@ public class DiaryCipher {
         }
     }
 
+    // Encrypting text based upon encrypting type
     public byte [] encryptText(String dataToEncrypt, SecretKey myKey, Cipher cipher){
 
         try {
@@ -51,6 +55,7 @@ public class DiaryCipher {
 
     }
 
+    // Decrypting bytes based upon encrypting type
     public String decryptString(byte [] dataToDecrypt, SecretKey myKey, Cipher cipher){
         try{
             cipher.init(Cipher.DECRYPT_MODE, myKey);
